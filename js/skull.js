1192fd434c09d1d5b40a9d9e49de17458409343f
Kang Chen <kang@airtasker.com>
2015-11-11 17:24:11 +1100

    skulls

---
+++ js/skull.js
@@ -0,0 +1,33 @@
+{
+  "metadata": {
+      "faces": 60339,
+      "sourceFile": "skull.obj",
+      "generator": "io_three",
+      "uvs": 0,
+      "bones": 0,
+      "morphTargets": 0,
+      "version": 3,
+      "normals": 30218,
+      "vertices": 30218,
+      "materials": 1
+  },
+  "materials": [{
+      "opacity": 1,
+      "specularCoef": 50,
+      "depthWrite": true,
+      "colorAmbient": [0.545882,0.533333,0.48],
+      "transparent": false,
+      "doubleSided": true,
+      "DbgName": "Skull",
+      "depthTest": true,
+      "DbgColor": 15658734,
+      "colorEmissive": [0.018724,0.018293,0.016464],
+      "DbgIndex": 0,
+      "colorDiffuse": [0.545882,0.533333,0.48],
+      "blending": "NormalBlending",
+      "visible": true,
+      "shading": "phong",
+      "colorSpecular": [0.5,0.5,0.5],
+      "wireframe": false
+  }]
+}
